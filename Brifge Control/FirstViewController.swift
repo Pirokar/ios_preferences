6103def98ba373a16b19e7705a479b8dcc077095
Владимир Рыбалка <vrybalka@initium.ru>
2017-08-18 12:42:18 +0300

    Initial Commit

---
+++ Brifge Control/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Brifge Control
+//
+//  Created by Владимир Рыбалка on 18/08/2017.
+//  Copyright © 2017 Vladimir Rybalka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
