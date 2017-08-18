1b182d808cf99f57e9bf62a5c7cbc4c2cf629d81
Владимир Рыбалка <vrybalka@initium.ru>
2017-08-18 16:54:50 +0300

    Implemented first window

--- Brifge Control/FirstViewController.swift
+++ Brifge Control/FirstViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet var officerLabel: UILabel!
+    @IBOutlet var authorizationCodeLabel: UILabel!
+    @IBOutlet var rankLabel: UILabel!
+    @IBOutlet var warpDriveLabel: UILabel!
+    @IBOutlet var warpFactorLabel: UILabel!
+    @IBOutlet var favoriteTeaLabel: UILabel!
+    @IBOutlet var favoriteCaptainLabel: UILabel!
+    @IBOutlet var favoriteGadgetLabel: UILabel!
+    @IBOutlet var favoriteAlienLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshFields()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +35,18 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func refreshFields() {
+        let defaults = UserDefaults.standard
+        officerLabel.text = defaults.string(forKey: officerKey)
+        authorizationCodeLabel.text = defaults.string(forKey: authorizationCodeKey)
+        rankLabel.text = defaults.string(forKey: rankKey)
+        warpDriveLabel.text = defaults.bool(forKey: warpDriveKey) ? "Включен" : "Выключен"
+        warpFactorLabel.text = (defaults.object(forKey: warpFactorKey) as AnyObject).stringValue
+        favoriteTeaLabel.text = defaults.string(forKey: favoriteTeaKey)
+        favoriteCaptainLabel.text = defaults.string(forKey: favoriteCaptainKey)
+        favoriteGadgetLabel.text = defaults.string(forKey: favoriteGadgetKey)
+        favoriteAlienLabel.text = defaults.string(forKey: favoriteAlienKey)
+    }
 
 }
 
